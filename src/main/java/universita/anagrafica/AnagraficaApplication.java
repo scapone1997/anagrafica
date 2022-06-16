78140d29dc1c8c74c0ca33084b3c2e91376c5b03
Capone.s <S.Capone@lutech.it>
2022-06-16 11:49:46 +0200

    spring init

---
+++ src/main/java/universita/anagrafica/AnagraficaApplication.java
@@ -0,0 +1,13 @@
+package universita.anagrafica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnagraficaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnagraficaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/universita/anagrafica/AnagraficaApplicationTests.java
@@ -0,0 +1,13 @@
+package universita.anagrafica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnagraficaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
